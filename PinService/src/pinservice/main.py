daf70709e7b793afb830146fa638a43c18e58868
Nokotachi <none@none.none>
2025-11-14 15:39:17 +0300

    не работающее подключение к s3

--- PinService/src/pinservice/main.py
+++ PinService/src/pinservice/main.py
@@ -4,6 +4,7 @@ from fastapi import Depends, FastAPI, File, Form, UploadFile
 
 from .database import get_session
 from .models import Pin
+from .reopositories.s3storage import s3_client
 from .schemas import PinCreate
 
 
@@ -16,5 +17,6 @@ app = FastAPI(lifespan=lifespan)
 
 
 @app.post("/")
-async def none():
+async def none(file: UploadFile, user_id=Form(), title=Form(), description=Form()):
+    await s3_client.upload_pin_image(user_id=user_id, file=file)
     return {"message": "Hello World"}
